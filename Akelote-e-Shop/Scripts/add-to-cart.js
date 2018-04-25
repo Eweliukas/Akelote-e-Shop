d6f51aee4ebb2603d02368f1fa6878b7f9d0bd43
Ignas Rudaitis <ignas.rudaitis@gmail.com>
2018-04-25 18:54:44 +0300

    Fix presentation logic for adding to cart

--- Akelote-e-Shop/Scripts/add-to-cart.js
+++ Akelote-e-Shop/Scripts/add-to-cart.js
@@ -1,6 +1,6 @@
 ﻿$(function () {
     $('.add-to-cart').click(function () {
-        var row = $(this).closest('tr');
+        var row = $(this).closest('.catalog-item');
         $.post('/ShoppingCart/AddToCart/' + $(this).data('id'))
             .done(function () {
                 row
@@ -10,7 +10,7 @@
                         left: row.offset().left,
                         top: row.offset().top
                     })
-                    .appendTo(row.closest('tbody'))
+                    .appendTo(row.closest('.catalog'))
                     .animate({
                         left: 0,
                         top: 0,
