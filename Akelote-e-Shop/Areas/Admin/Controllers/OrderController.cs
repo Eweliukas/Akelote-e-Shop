bbd2303bb7cfc41c353ce850911d37cc68ec593a
Vytautas Saulis <vsaulis@trackforce.com>
2018-05-29 23:51:06 +0300

    Users, orders crud

---
+++ Akelote-e-Shop/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Akelote_e_Shop.Models;
+
+namespace Akelote_e_Shop.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Order
+        public ActionResult Index() {
+            var order = db.Order.Include(c => c.OrderItems);
+            return View(order.ToList());
+        }
+
+        // GET: Admin/Order/Details/5
+        public ActionResult Details(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Order.Find(id);
+            if (order == null) {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+    }
+}
---
+++ Akelote-e-Shop/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Akelote_e_Shop.Models;
+
+namespace Akelote_e_Shop.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/User
+        public ActionResult Index() {
+            var user = db.Users.Include(c => c.Orders);
+            return View(user.ToList());
+        }
+
+        // GET: Admin/User/Details/5
+        public ActionResult Details(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var user = db.Users.Find(id);
+            if (user == null) {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+    }
+}
