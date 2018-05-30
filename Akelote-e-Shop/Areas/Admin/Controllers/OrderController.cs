6986130eb468af4e9a42013dabe4e44734cd4334
Vytautas Saulis <vsaulis@trackforce.com>
2018-05-30 16:09:03 +0300

    User all

--- Akelote-e-Shop/Areas/Admin/Controllers/OrderController.cs
+++ Akelote-e-Shop/Areas/Admin/Controllers/OrderController.cs
@@ -31,7 +31,7 @@ namespace Akelote_e_Shop.Areas.Admin.Controllers
             return View(order);
         }
 
-        // GET: Admin/Property/Delete/5
+        // GET: Admin/Order/Complete/5
         public ActionResult Complete(int? id) {
             if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
@@ -43,12 +43,34 @@ namespace Akelote_e_Shop.Areas.Admin.Controllers
             return View(order);
         }
 
-        // POST: Admin/Property/Delete/5
+        // POST: Admin/Order/Complete/5
         [HttpPost, ActionName("Put")]
         [ValidateAntiForgeryToken]
         public ActionResult CompleteConfirmed(int id) {
             Order order = db.Order.Find(id);
             order.Status = OrderStatus.Completed;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Admin/Order/Cancel/5
+        public ActionResult Cancel(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Order.Find(id);
+            if (order == null) {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Admin/Order/Cancel/5
+        [HttpPost, ActionName("Put")]
+        [ValidateAntiForgeryToken]
+        public ActionResult CancelConfirmed(int id) {
+            Order order = db.Order.Find(id);
+            order.Status = OrderStatus.Cancelled;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
--- Akelote-e-Shop/Areas/Admin/Controllers/UserController.cs
+++ Akelote-e-Shop/Areas/Admin/Controllers/UserController.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Akelote_e_Shop.Models;
+using Microsoft.VisualBasic.ApplicationServices;
 
 namespace Akelote_e_Shop.Areas.Admin.Controllers
 {
@@ -29,6 +30,72 @@ namespace Akelote_e_Shop.Areas.Admin.Controllers
                 return HttpNotFound();
             }
             return View(user);
+        }
+
+        // GET: Admin/User/Delete/5
+        public ActionResult Delete(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var user = db.Users.Find(id);
+            if (user == null) {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Admin/User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id) {
+            var user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Admin/User/Block/5
+        public ActionResult Block(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var user = db.Users.Find(id);
+            if (user == null) {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Admin/User/Block/5
+        [HttpPost, ActionName("Put")]
+        [ValidateAntiForgeryToken]
+        public ActionResult BlockConfirmed(int id) {
+            var user = db.Users.Find(id);
+            user.IsBlocked = true;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Admin/User/Unblock/5
+        public ActionResult Unblock(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var user = db.Users.Find(id);
+            if (user == null) {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Admin/User/Unblock/5
+        [HttpPost, ActionName("Put")]
+        [ValidateAntiForgeryToken]
+        public ActionResult UnblockConfirmed(int id) {
+            var user = db.Users.Find(id);
+            user.IsBlocked = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
